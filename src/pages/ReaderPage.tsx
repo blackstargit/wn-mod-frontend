655fa7de889165c204b4931a994e625f7facbd8a
blackstargit <blackst1rmail@gmail.com>
2026-02-17 10:21:40 +0500

    fixed fixed settings and tts player issue

--- src/pages/ReaderPage.tsx
+++ src/pages/ReaderPage.tsx
@@ -139,7 +139,7 @@ const ReaderPage: React.FC = () => {
   }
 
   return (
-    <>
+    <div className="relative min-h-screen">
       <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto:wght@400;500;700&family=Open+Sans:wght@400;600;700&family=Lato:wght@400;700&family=Montserrat:wght@400;600;700&family=Poppins:wght@400;500;600;700&family=Merriweather:wght@400;700&family=Playfair+Display:wght@400;700&family=Lora:wght@400;700&family=PT+Serif:wght@400;700&family=Crimson+Text:wght@400;600;700&family=Source+Sans+Pro:wght@400;600;700&family=Raleway:wght@400;600;700&family=Ubuntu:wght@400;500;700&family=Nunito:wght@400;600;700&family=Quicksand:wght@400;500;700&family=Josefin+Sans:wght@400;600;700&family=Bitter:wght@400;700&family=Libre+Baskerville:wght@400;700&family=EB+Garamond:wght@400;700&family=Noto+Serif:wght@400;700&display=swap"
         rel="stylesheet"
@@ -294,7 +294,7 @@ const ReaderPage: React.FC = () => {
 
       {/* TTS Player Popup */}
       <TTSPlayer />
-    </>
+    </div>
   );
 };
 
